26b124508dfde2b9d5416a773afe0f82df2cc4cd
PedroGimenes <pedro.gimenes@etec.sp.gov.br>
2025-08-20 15:18:05 -0300

    terminando

--- perfil-usuario/src/App.js
+++ perfil-usuario/src/App.js
@@ -9,9 +9,9 @@ function App() {
          <h1> Exercicio componentes</h1>
           <Header/>
           <Cartao
-           imagem= "https://abcinecursos.org.br/wp-content/uploads/2024/06/ATE-ONDE-ELA-VAI.webp"
-           nome="ATÉ ONDE ELA VAI"
-           descrição= "Durante a infância, Bárbara teve experiências paranormais bem estranhas, ouvindo vozes e vendo vultos. Já adulta, o medo ainda é um fator presente, pois tem pavor que os episódios sofridos quando criança retornem para sua vida."
+           imagem= "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/607610a4-2f53-4578-8b39-8984af0ed614/dgxmyyr-88f48427-b98e-49b6-a801-6be9ee2df2c5.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzYwNzYxMGE0LTJmNTMtNDU3OC04YjM5LTg5ODRhZjBlZDYxNFwvZGd4bXl5ci04OGY0ODQyNy1iOThlLTQ5YjYtYTgwMS02YmU5ZWUyZGYyYzUuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.XQbFeM4caTCAWPadYARvcUwHgB6oyJQ8AzQUXcBEbHA"
+           nome="Palhaço"
+           descrição= "Um palhaço é um artista cômico que utiliza técnicas teatrais, como maquiagem, figurino e improvisação, para entreter e provocar risos. Eles podem atuar em circos, festas, hospitais e outros ambientes, adaptando suas performances para diferentes públicos."
           />
           <Footer/>
           
